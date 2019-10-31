eafaf804f4884ea9cd81c28a4ec68f59fc1cf9bc
ian.park <ian.park@navercorp.com>
2019-10-31 21:43:27 +0900

    feat: apply sort logic for special keyword

--- src/utilities/formatSdl.js
+++ src/utilities/formatSdl.js
@@ -5,10 +5,30 @@ import {
   print,
   parse,
 } from 'graphql';
+import {
+  isSpecialkeyword,
+  specialKeywordCompare,
+} from './isSpecialKeyword';
 
 export default (schemaSdl: string): string => {
   return print(mapObject(parse(schemaSdl), (key, value) => {
-    if ((key === 'fields' || key === 'definitions') && Array.isArray(value)) {
+    if (key === 'definitions' && Array.isArray(value)) {
+      return [
+        key,
+        value.slice().sort((a, b) => {
+          const left = a.name.value;
+          const right = b.name.value;
+
+          if (isSpecialkeyword(left) || isSpecialkeyword(right)) {
+            return specialKeywordCompare(left, right);
+          }
+
+          return left.localeCompare(right);
+        }),
+      ];
+    }
+
+    if (key === 'fields' && Array.isArray(value)) {
       return [
         key,
         value.slice().sort((a, b) => {
